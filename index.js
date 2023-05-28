17e83ca4925f0e470a9e3a1ac3a749729a1b5d2e
NodeMixaholic <9904667+NodeMixaholic@users.noreply.github.com>
2023-05-28 21:10:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ 
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGES
-  ]
+const client = new Discord.Client({
+  ws: { 
+    intents: [
+      'GUILDS',
+      'GUILD_MEMBERS',
+      'GUILD_MESSAGES'
+    ]
+  }
 });
 const db = require('quick.db');
 
