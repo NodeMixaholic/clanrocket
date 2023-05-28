e98ea861098782a7f14573c2796c14f07e03f3c3
NodeMixaholic <9904667+NodeMixaholic@users.noreply.github.com>
2023-05-28 22:00:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ client.on('messageCreate', async (message) => {
     message.channel.send(`You are currently at level ${userLevel} with ${userXP} XP.`);
   }
   else if (command === 'setlevel') {
-    if (!message.member.permissions.has("ADMINISTRATOR")) {
+    if (!message.member.permissions.has("KICK_MEMBERS")) {
       message.channel.send('Only administrators can use this command.');
       return;
     }
